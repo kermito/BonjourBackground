aebdff9bd50e032066866ba126d4eb5905c1a13f
Arnaud <triolet.arnaud@gmail.com>
2016-04-09 19:19:54 +0200

    Support de Windows

--- background.py
+++ background.py
@@ -5,22 +5,40 @@ def getBackgound(page):
     imageItem = soup.find("div",attrs={"class":"photo"}).find("img")
     return(imageItem['src'])
 
+def setGnomeBackground(imagePath):
+	import os
+	os.system("gsettings set org.gnome.desktop.background draw-background false;")
+	command = 'gsettings set org.gnome.desktop.background picture-uri "'+imagePath+'"'
+	os.system(command)
+	os.system("gsettings set org.gnome.desktop.background draw-background true;")
+
+def setWindowsBackground(imagePath):
+	command = 'reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "'+imagePath+'" /f'
+	os.system(command)
+	os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
+
 #getting some sexy page
-import urllib
-content = urllib.urlopen("http://dites.bonjourmadame.fr/").read()
+import urllib.request
+content = urllib.request.urlopen("http://dites.bonjourmadame.fr/").read()
 url = getBackgound(content)
 
 #Downloading the sexy girl
-urllib.urlretrieve(url, "Image.jpg")
+import urllib
+urllib.request.urlretrieve(url, "Image.jpg")
 import os.path
 root = os.path.abspath("")
-basePath= root+"/Image.jpg"
-bmpPath= root+"/Image.bmp"
+basePath = root+"/Image.jpg"
+bmpPath = root+"/Image.bmp"
 #print(basePath)
 
 #Lap dance
-import os
-os.system("gsettings set org.gnome.desktop.background draw-background false;")
-command = 'gsettings set org.gnome.desktop.background picture-uri "'+basePath+'"'
-os.system(command)
-os.system("gsettings set org.gnome.desktop.background draw-background true;")
+import platform
+system = platform.system()
+print(system)
+if system == "Windows":
+	setWindowsBackground(basePath)
+elif system  == 'linux':
+	from PIL import Image
+	Image.open(basePath).save(bmpPath)
+	setGnomeBackground(bmpPath)
+	pass
