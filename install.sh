cb647f3ef294e4c49f5acad8a8c03b9adf284dce
Arnaud <triolet.arnaud@gmail.com>
2018-03-20 09:51:23 +0100

    Auto stash before merge of "master" and "origin/master"

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ FOLDER=$(pwd)
 printf "[Desktop Entry]\n" > ~/.config/autostart/BonjourBackground.desktop
 printf "Type=Application\n" >> ~/.config/autostart/BonjourBackground.desktop
 printf "Name=BonjourBackground\n" >> ~/.config/autostart/BonjourBackground.desktop
-printf "Exec=\"python %s/background.py\"\n" "$FOLDER" >> ~/.config/autostart/BonjourBackground.desktop
-printf "Icon=\"%s/icon.png\"\n" "$FOLDER" >> ~/.config/autostart/BonjourBackground.desktop
-printf "Comment=\"Every day a new background\"\n" >> ~/.config/autostart/BonjourBackground.desktop
+printf "Exec=python %s/background.py\n" "$FOLDER" >> ~/.config/autostart/BonjourBackground.desktop
+printf "Icon=%s/icon.png\n" "$FOLDER" >> ~/.config/autostart/BonjourBackground.desktop
+printf "Comment=Every day a new background\n" >> ~/.config/autostart/BonjourBackground.desktop
 printf "X-GNOME-Autostart-enabled=true\n" >> ~/.config/autostart/BonjourBackground.desktop
 # > ~/.config/autostart/BonjourBackground.desktop
